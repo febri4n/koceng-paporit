a4a0a9779d0bb98d1388f78986dbef3a933c7e8d
febri4n <solihinfebrian0@gmail.com>
2022-09-02 23:41:33 +0700

    update favorite button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import fluffykins from './images/fluffykins.png'
+import { BsMailbox2, BsTelephoneFill, BsStar, BsStarFill } from "react-icons/bs";
+import React from 'react';
 
 function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "Fluffykins",
+    lastName: "Gemoy",
+    phone: "(212) 555-2345",
+    email: "fluff@me.com",
+    isFavorite: true
+  })
+
+  function toggleFavorite() {
+    setContact(prevContact => {
+      // return object
+      return ({
+        // kenapa menggunakan ...prevContact ? Karena jika tidak maka state akan mengubah menjadi 1 property saja yaitu isFavorite dan property yang lainnya akan hilang. Sedangkan jika menggunakan ...prevContact akan mengoveride property.
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contacts'>
+      <div className='contact-card'>
+        <img src={fluffykins} alt='gambar koceng' />
+        <h2>{`${contact.firstName} ${contact.lastName}`} {contact.isFavorite ? <BsStar className='icon' size={23} onClick={toggleFavorite} /> : <BsStarFill className='icon' size={23} onClick={toggleFavorite} />} </h2>
+        <div className='info-group'>
+          <BsTelephoneFill size={23} />
+          <p>{contact.phone}</p>
+        </div>
+        <div className='info-group'>
+          <BsMailbox2 size={23} />
+          <p>{contact.email}</p>
+        </div>
+      </div>
     </div>
   );
 }
